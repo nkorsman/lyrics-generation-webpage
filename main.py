5b0cc0164afa82bcaeb7d40da4c4aee0e13a29af
Daniel Holmberg <daniel.holmberg97@gmail.com>
2019-10-14 14:34:01 +0300

    Test fix

--- main.py
+++ main.py
@@ -1,26 +1,26 @@
 from flask import Flask
-from flask import url_for, jsonify, render_template
+from flask import url_for, render_template
 from textgenrnn import textgenrnn
 #import warnings
 #warnings.filterwarnings("ignore")
 
 app = Flask(__name__)
 
-def load_model():
-    global textgen
-    textgen = textgenrnn("textgenrnn_weights_4epochs.hdf5")
+#def load_model():
+#    global textgen
+#    textgen = textgenrnn("textgenrnn_weights_4epochs.hdf5")
 
 @app.route('/')
 def index():
     return render_template('index.html')
 
 
-@app.route('/foo', methods=['GET', 'POST'])
+@app.route('/foo', methods=['POST'])
 def foo():
+    textgen = textgenrnn("textgenrnn_weights_4epochs.hdf5")
     lyrics = textgen.generate(n=20, return_as_list=True)
     return "\n".join(lyrics)
-    #return jsonify({"message": "you're a superstar"})
 
 if __name__ == "__main__":
-    load_model()
-    app.run()
+    #load_model()
+    app.run(threaded=False)
