a3dc16109f531cb51a854f559b02fad101e842ac
Daniel Holmberg <daniel.holmberg97@gmail.com>
2019-10-14 19:51:53 +0300

    CSS added

--- main.py
+++ main.py
@@ -1,19 +1,17 @@
 from flask import Flask
-from flask import url_for, render_template
+import os
+from flask import url_for, render_template, send_from_directory
 from textgenrnn import textgenrnn
-#import warnings
-#warnings.filterwarnings("ignore")
 
 app = Flask(__name__)
 
-#def load_model():
-#    global textgen
-#    textgen = textgenrnn("textgenrnn_weights_4epochs.hdf5")
+@app.route('/favicon.ico') 
+def favicon(): 
+    return send_from_directory(os.path.join(app.root_path, 'static'), 'favicon.ico', mimetype='image/vnd.microsoft.icon')
 
 @app.route('/')
 def index():
     return render_template('index.html')
-
 
 @app.route('/foo', methods=['POST'])
 def foo():
@@ -22,5 +20,4 @@ def foo():
     return "\n".join(lyrics)
 
 if __name__ == "__main__":
-    #load_model()
     app.run(threaded=False)
